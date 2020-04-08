040bbf7b64099530ecfb183ada57c71ec82f3f7f
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-04-08 16:43:58 -0500

    made edits

--- src/javascripts/components/projects.js
+++ src/javascripts/components/projects.js
@@ -5,8 +5,8 @@ const projectCardBuilder = () => {
   const projects = projectData.getProjects();
   let domString = '';
   projects.forEach((project) => {
-    domString += '<div class="card col-lg-5" id="project-cards" style="width: 18rem;">';
-    domString += `<img src=${project.screenshot} class="card-img-top project-cards" alt="...">`;
+    domString += '<div class="card mb-5 col-lg-5" id="project-cards" style="width: 18rem;">';
+    domString += `<img src=${project.screenshot} class="card-img-top project-cards m-0" alt="...">`;
     domString += '<div class="card-body">';
     domString += `<h5 class="card-title">${project.title}</h5>`;
     // domString += `<p class="card-text">${project.description}</p>`;
