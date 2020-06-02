1532249314760c4a40dacfcb30a2f369f488d3bb
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-06-02 01:59:44 -0500

    deploy

--- src/helpers/data/projectData.js
+++ src/helpers/data/projectData.js
@@ -2,8 +2,8 @@ const projects = [
   {
     title: 'Tamagotchi',
     screenshot: 'https://i.imgur.com/PkkLaUy.png',
-    description: 'For this project, we created our very own version of the first virtual game to ever exist, the Tamagotchi. This project allowed us to practice using SASS. We only used SASS for all of the styling for this project. Our Tamagotchi game included four quadrants, each for a different feature in our game. Each quadrant included a real live progress bar that updated after each event was clicked.', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: 'HTML, CSS, SASS, Vanilla JavaScript, Version Control with Github',
+    description: 'A recreation of the classic hit kids virtual game, Tamagotchi. This project allowed us to practice using SASS. We only used SASS for all of the styling for this project. Our Tamagotchi game included four quadrants, each for a different feature in our game. Each quadrant included a real live progress bar that updated after each event was clicked.', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: 'HTML, CSS, SASS, Vanilla JavaScript, jQuery, Github',
     available: true,
     url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: 'https://github.com/stephencastaneda/tamagotchiproject',
@@ -12,37 +12,28 @@ const projects = [
     title: 'Textual Healing App',
     screenshot: 'https://i.imgur.com/FbHoSdt.png',
     description: 'This project is a basic messaging app that allows you to create messages and delete messages. This app also features a dark mode and large text option. This project utilized a plethora of vanilla javascript methods. Our app uses seed data that displays on page load. There are radio buttons to select a user before typing your message. After you type your message, there is a button that allows you to delete your message.',
-    technologiesUsed: 'HTML, CSS, SASS, Vanilla JavaScript, Version Control with Github',
-    available: false,
+    technologiesUsed: 'HTML, CSS, SASS, Vanilla JavaScript, jQuery, Github',
+    available: true,
     url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: 'https://github.com/nss-evening-cohort-11/chatty-butterworts',
   },
   {
-    title: 'Cool Project',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: false,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    title: 'Star Destroyer',
+    screenshot: 'https://i.imgur.com/oVpJEF0.png',
+    description: 'A project that features all missions, personnel, weapons, enemies, and sectors in our very own Star Wars galaxy. Users are able to create a mission. As well as edit the mission planetery sector and enemy target. Users can also add personnel and weapons for their missions.', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: 'HTML, CSS, VS Code, React, Bootstrap, jQuery, firebase, Github',
+    available: true,
+    url: 'https://star-wars-484f4.web.app', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: 'https://github.com/nss-evening-cohort-11/nutshell-ewoks-droids',
   },
   {
-    title: 'Cool Project',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: false,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  },
-  {
-    title: 'Cool Project',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    title: 'Sports Roster',
+    screenshot: 'https://i.imgur.com/Gfr02Cm.png',
+    description: 'This is the second app I created with React. This project features a team roster for the Los Angeles Lakers. Users can create, edit, and add players as they wish.', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: 'HTML, SASS, VS Code, Github, Bootstrap, jQuery, firebase, Github',
     available: true,
     url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    githubUrl: 'https://github.com/stephencastaneda/sports-roster',
   },
 ];
 
--- src/javascripts/components/projects.js
+++ src/javascripts/components/projects.js
@@ -1,6 +1,10 @@
 import projectData from '../../helpers/data/projectData';
 import utils from '../../helpers/utils';
 
+const popupInit = () => {
+  $('[data-toggle="popover"]').popover();
+};
+
 const projectCardBuilder = () => {
   const projects = projectData.getProjects();
   let domString = '';
@@ -10,6 +14,15 @@ const projectCardBuilder = () => {
     domString += '<div class="card-body">';
     domString += `<h5 class="card-title">${project.title}</h5>`;
     // domString += `<p class="card-text">${project.description}</p>`;
+    // domString += '<div class="overlay">';
+    // domString += `${project.description}`;
+    // domString += '<div>';
+    domString += '<div class="overlay">';
+    domString += `<div class="description-text"><span>${project.description}</span></div>`;
+    domString += `<button type="button" class="btn btn-secondary popover-button" data-trigger="focus" data-container="body" data-toggle="popover" data-placement="right" data-content="${project.technologiesUsed}">`;
+    domString += 'TECH USED';
+    domString += '</button>';
+    domString += '</div>';
     domString += '<button class="btn btn-dark website-link-btn mr-2"><i class="fas fa-globe"></i></a></button>';
     domString += `<a class="btn btn-dark git-hub-btn" href="${project.githubUrl}"><i class="fab fa-github"></i></a>`;
     domString += '</div>';
@@ -17,6 +30,7 @@ const projectCardBuilder = () => {
   });
 
   utils.printToDom('projects-page', domString);
+  $('body').on('click', '.popover-button', popupInit);
 };
 
 export default { projectCardBuilder };
